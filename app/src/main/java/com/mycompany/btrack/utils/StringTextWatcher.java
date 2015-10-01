c5b21375de230a750420e6602985b34dcfa62979
michael wang <michael_ye1992@hotmail.com>
2015-10-02 03:12:22 +1000

    merging

--- app/src/main/java/com/mycompany/btrack/utils/StringTextWatcher.java
+++ app/src/main/java/com/mycompany/btrack/utils/StringTextWatcher.java
@@ -9,26 +9,15 @@ import android.widget.Toast;
 /**
  * Created by JCBSH on 29/09/2015.
  */
-//<<<<<<< HEAD:app/src/main/java/com/mycompany/btrack/utils/DONOTUSEStringTextWatcher.java
-//public class DONOTUSEStringTextWatcher implements TextWatcher {
-//    public final String TAG = DONOTUSEStringTextWatcher.class.getSimpleName();
-//    private final EditText mAmount;
-//=======
 public class StringTextWatcher implements TextWatcher {
     public final String TAG = StringTextWatcher.class.getSimpleName();
     private final EditText mEditText;
-//>>>>>>> c11989de0f07eb0d32eae8ab4151dd0c774b759d:app/src/main/java/com/mycompany/btrack/utils/StringTextWatcher.java
     private int mSizeLimit;
     private String mBefore;
     private Toast mToast;
 
-//<<<<<<< HEAD:app/src/main/java/com/mycompany/btrack/utils/DONOTUSEStringTextWatcher.java
-//    public DONOTUSEStringTextWatcher (EditText amount, int sizeLimit) {
-//        mAmount = amount;
-//=======
     public StringTextWatcher (EditText editText, int sizeLimit) {
         mEditText = editText;
-//>>>>>>> c11989de0f07eb0d32eae8ab4151dd0c774b759d:app/src/main/java/com/mycompany/btrack/utils/StringTextWatcher.java
         mSizeLimit = sizeLimit;
         mToast = Toast.makeText(mEditText.getContext(), "blah", Toast.LENGTH_SHORT);
         mToast.setGravity(Gravity.CENTER, 0, 0);
