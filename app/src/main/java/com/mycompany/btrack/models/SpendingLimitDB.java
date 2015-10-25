1eac9e4f27bcc63e96a627c2d1d626727227202b
atician <godfrey.truong.2008@gmail.com>
2015-10-25 14:50:46 +1100

    added defailt cstor

--- app/src/main/java/com/mycompany/btrack/models/SpendingLimitDB.java
+++ app/src/main/java/com/mycompany/btrack/models/SpendingLimitDB.java
@@ -7,6 +7,9 @@ public class SpendingLimitDB {
 
     private double amount;
 
+    public SpendingLimitDB() {
+    }
+
     public SpendingLimitDB(double amount) {
         this.amount = amount;
     }
